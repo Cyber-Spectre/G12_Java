cec761e658b4c6d6bd6c9ec39a4a0934ba57cd6e
Spectre <52917568+Cyber-Spectre@users.noreply.github.com>
2022-11-30 15:30:08 -0700

    Add files via upload

--- Final_Project/MinesweeperTextVer.java
+++ Final_Project/MinesweeperTextVer.java
@@ -44,36 +44,25 @@ public class MinesweeperTextVer {
   }
 
   public static void setup(int x, int y, int mines, String[][] gameInfo, String[][] UI) { //?sets up arrays
-    for(int a = 0; a < x; a++) { //prepares UI
-      for(int b = 0; b < y; b++) {
+    int a, b, a1, a2, b1, b2;
+    for(a = 0; a < x; a++) { //prepares UI
+      for(b = 0; b < y; b++) {
         UI[a][b] = "O";
       }
     }
 
+    displayUI(x, y, UI);
+
     System.out.println("\nChoose a cell to start in (ex: C08):\n");
     String inputStr = scan1.next();
-    int a = coordX(inputStr);
-    int b = coordY(inputStr);
-    int a1, b1, a2, b2;
-    if(a == 0) {
-      a1 = 0;
-    } else {
-      a1 = a - 1;
-    } 
-    if(b == 0) {
-      b1 = 0;
-    } else {
-      b1 = b - 1;
-    } 
-    if(a == x - 1) {
-      a2 = x - 1;
-    } else {
-      a2 = a + 1;
-    } 
-    if(b == y - 1) {
-      b2 = y - 1;
-    } else {
-      b2 = b + 1;
+    int borderX = coordX(inputStr);
+    int borderY = coordY(inputStr);
+    for(a = 0; a < x; a++) { //sets safe zone around starting point
+      for(b = 0; b < y; b++) {
+        if((a >= (borderX - 1) && a <= (borderX + 1)) && (b >= (borderY - 1) && b <= (borderY + 1))) {
+          gameInfo[a][b] = "X";
+        }
+      }
     }
 
     for(a = 0; a < x; a++) { //prepares gameInfo
@@ -83,6 +72,7 @@ public class MinesweeperTextVer {
         }
       }
     }
+    displayInfo(x, y, gameInfo);
 
     for(int c = 0; c < mines; c++) { //places mines
       a = rand.nextInt(x);
@@ -127,12 +117,15 @@ public class MinesweeperTextVer {
         }
       }
     }
+
+    reveal(x, y, borderX, borderY, UI, gameInfo);
   }
 
   public static void setStart(int a1, int a2, int b1, int b2, String[][] gameInfo) { //?sets up gameInfo to ensure start area is safe
     for (int x = a1; x <= a2; x++) {
       for (int y = b1; x <= b2; y++) {
         gameInfo[x][y] = "X";
+      }
     }
   }
 
@@ -149,7 +142,7 @@ public class MinesweeperTextVer {
   }
 
   public static int coordX(String input) { //?returns X coordinate
-    int xInt = Integer.valueOf(input.substring(2, 3));
+    int xInt = Integer.valueOf(input.substring(1, 3));
     return xInt - 1;
   }
 
@@ -191,8 +184,52 @@ public class MinesweeperTextVer {
     } 
   }
 
-  public static void reveal(int x, int y, String[][] UI, String[][] gameInfo) {
+  public static void reveal(int x, int y, int a, int b, String[][] UI, String[][] gameInfo) {
+    String isRevealed = "";
+    for(int c = 0; c < x; c++) { //places number values
+      for(int d = 0; d < y; d++) {
+        if((c >= (a - 1) && c <= (a + 1)) && (d >= (b - 1) && d <= (b + 1))) {
+          UI[c][d] = gameInfo[c][d];
+          if(UI[c][d] == " " && !isRevealed(c, d, isRevealed)) {
+            isRevealed = isRevealed + "(" + c + " " + d + ")";
+            reveal(x, y, c, d, UI, gameInfo, isRevealed);
+          }
+        }
+      }
+    }
+  }
 
+  public static boolean isRevealed(int c, int d, String isRevealed) {
+    for(int x = 0; x > isRevealed.length();) {
+    String xCheck = "";
+    String yCheck = "";
+    int xVal;
+    int yVal;
+      if(isRevealed.charAt(x) != '(') {
+        x++;
+      } else {
+        for(int y = (x + 1); isRevealed.charAt(y) != ' ';) {
+          xCheck = xCheck + isRevealed.charAt(y);
+          x++;
+        }
+        xVal = Integer.valueOf(xCheck.substring(0));
+        for(int y = (x + 1); isRevealed.charAt(y) != ')';) {
+          yCheck = yCheck + isRevealed.charAt(y);
+          x++;
+        }
+        yVal = Integer.valueOf(yCheck.substring(0));
+        if(xVal == c && yVal == d) {
+          return true;
+        } else {
+          x++;
+        }
+      }
+    }
+    return false;
+  }
+
+  public static void reveal(int x, int y, int a, int b, String[][] UI, String[][] gameInfo, String alrRevealed) {
+    
   }
 
   public static void display() {
