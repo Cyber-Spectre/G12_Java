3b6c5d55092ce5831657617e32df59be8e3bd9e3
Spectre <52917568+Cyber-Spectre@users.noreply.github.com>
2022-12-15 12:03:59 -0700

    Add files via upload

--- Final_Project/Minesweeper/GUI.java
+++ Final_Project/Minesweeper/GUI.java
@@ -1,5 +1,16 @@
 package Final_Project.Minesweeper;
 
-public class GUI {
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+
+public class GUI extends JFrame {
+
+  public GUI() {
+    this.setTitle("Minesweeper");
+    this.setSize(1286, 829);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setVisible(true);
+  }
   
 }
